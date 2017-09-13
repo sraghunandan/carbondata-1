5b2cc4b2e5d067ef16cd2a26adce27a499c4ce2a
sraghunandan <carbondatacontributions@gmail.com>
2017-09-13 19:55:21 +0530

    improve coverage

--- integration/spark-common-test/src/test/scala/org/apache/carbondata/integration/spark/testsuite/aggquery/IntegerDataTypeTestCase.scala
+++ integration/spark-common-test/src/test/scala/org/apache/carbondata/integration/spark/testsuite/aggquery/IntegerDataTypeTestCase.scala
@@ -183,7 +183,7 @@ class IntegerDataTypeTestCase extends QueryTest with BeforeAndAfterAll {
     sql(
       """
         | CREATE TABLE short_int_target_table
-        | (begin_time bigint, name string)
+        | (begin_time bigint, name string,begin_time1 long,begin_time2 long,begin_time3 long,begin_time4 long,begin_time5 int,begin_time6 int,begin_time7 int,begin_time8 int)
         | STORED BY 'org.apache.carbondata.format'
       """.stripMargin)
 
@@ -308,7 +308,7 @@ class IntegerDataTypeTestCase extends QueryTest with BeforeAndAfterAll {
     sql(
       """
         | CREATE TABLE short_int_target_table
-        | (begin_time int, name string)
+        | (begin_time int, name string,begin_time1 long,begin_time2 long,begin_time3 long,begin_time4 long,begin_time5 int,begin_time6 int,begin_time7 int,begin_time8 int)
         | STORED BY 'org.apache.carbondata.format'
       """.stripMargin)
 
@@ -489,7 +489,131 @@ class IntegerDataTypeTestCase extends QueryTest with BeforeAndAfterAll {
 
   /*----------------------*/
 
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+  test("short int as target type in AdaptiveIntegerCodec") {
+    sql(
+      """
+        | DROP TABLE IF EXISTS short_int_target_table
+      """.stripMargin)
 
+    //begin_time column will be encoded by deltaIntegerCodec
+    sql(
+      """
+        | CREATE TABLE short_int_target_table
+        | (begin_time bigint, name string,begin_time1 long,begin_time2 long,begin_time3 long,begin_time4 long,begin_time5 int,begin_time6 int,begin_time7 int,begin_time8 int)
+        | STORED BY 'org.apache.carbondata.format'
+      """.stripMargin)
+
+    sql(
+      s"""
+         | LOAD DATA LOCAL INPATH '$resourcesPath/short_as_target_type.csv'
+         | INTO TABLE short_int_target_table
+      """.stripMargin)
+
+    checkAnswer(
+      sql("select begin_time from short_int_target_table"),
+      Seq(Row(1497376581), Row(1497423838))
+    )
+
+    sql(
+      """
+        | DROP TABLE short_int_target_table
+      """.stripMargin)
+  }
+
+  test("byte as target type in AdaptiveIntegerCodec") {
+    sql(
+      """
+        | DROP TABLE IF EXISTS short_int_target_table
+      """.stripMargin)
+
+    //begin_time column will be encoded by deltaIntegerCodec
+    sql(
+      """
+        | CREATE TABLE short_int_target_table
+        | (begin_time bigint, name string,begin_time1 long,begin_time2 long,begin_time3 long,begin_time4 long,begin_time5 int,begin_time6 int,begin_time7 int,begin_time8 int)
+        | STORED BY 'org.apache.carbondata.format'
+      """.stripMargin)
+
+    sql(
+      s"""
+         | LOAD DATA LOCAL INPATH '$resourcesPath/short_as_target_type.csv'
+         | INTO TABLE short_int_target_table
+      """.stripMargin)
+
+    checkAnswer(
+      sql("select begin_time from short_int_target_table"),
+      Seq(Row(1497376581), Row(1497423838))
+    )
+
+    sql(
+      """
+        | DROP TABLE short_int_target_table
+      """.stripMargin)
+  }
+
+  test("short as target type in AdaptiveIntegerCodec") {
+    sql(
+      """
+        | DROP TABLE IF EXISTS short_int_target_table
+      """.stripMargin)
+
+    //begin_time column will be encoded by deltaIntegerCodec
+    sql(
+      """
+        | CREATE TABLE short_int_target_table
+        | (begin_time bigint, name string,begin_time1 long,begin_time2 long,begin_time3 long,begin_time4 long,begin_time5 int,begin_time6 int,begin_time7 int,begin_time8 int)
+        | STORED BY 'org.apache.carbondata.format'
+      """.stripMargin)
+
+    sql(
+      s"""
+         | LOAD DATA LOCAL INPATH '$resourcesPath/short_as_target_type.csv'
+         | INTO TABLE short_int_target_table
+      """.stripMargin)
+
+    checkAnswer(
+      sql("select begin_time from short_int_target_table"),
+      Seq(Row(1497376581), Row(1497423838))
+    )
+
+    sql(
+      """
+        | DROP TABLE short_int_target_table
+      """.stripMargin)
+  }
+
+  test("int as target type in AdaptiveIntegerCodec") {
+    sql(
+      """
+        | DROP TABLE IF EXISTS short_int_target_table
+      """.stripMargin)
+
+    //begin_time column will be encoded by deltaIntegerCodec
+    sql(
+      """
+        | CREATE TABLE short_int_target_table
+        | (begin_time bigint, name string,begin_time1 long,begin_time2 long,begin_time3 long,begin_time4 long,begin_time5 int,begin_time6 int,begin_time7 int,begin_time8 short)
+        | STORED BY 'org.apache.carbondata.format'
+      """.stripMargin)
+
+    sql(
+      s"""
+         | LOAD DATA LOCAL INPATH '$resourcesPath/short_as_target_type.csv'
+         | INTO TABLE short_int_target_table
+      """.stripMargin)
+
+    checkAnswer(
+      sql("select begin_time from short_int_target_table"),
+      Seq(Row(1497376581), Row(1497423838))
+    )
+
+    sql(
+      """
+        | DROP TABLE short_int_target_table
+      """.stripMargin)
+  }
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
 
 
 
